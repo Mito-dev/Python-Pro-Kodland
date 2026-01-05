dbf3db3ca006ab4fbae305067eb1fd8d3be3e4e6
Mito-dev <michoefanly@gmail.com>
2026-01-05 19:36:54 +0700

    Create Meme_Dict.py

---
+++ Meme_Dict.py
@@ -0,0 +1,14 @@
+meme_dict = {
+            "CRINGE": "Sesuatu yang sangat aneh atau memalukan",
+            "LOL": "Tanggapan umum terhadap sesuatu yang lucu",
+            "SIGMA":"Sesuatu yang bagus/keren"
+            }
+
+word = input("Ketik kata yang tidak Kamu mengerti (gunakan huruf kapital semua!): ")
+
+if word in meme_dict.keys():
+    # Apa yang harus kita lakukan jika kata itu ditemukan?
+    print(meme_dict[word])
+else:
+    print("Kata tidak ada di kamus.")
+    # Apa yang harus kita lakukan jika kata itu tidak ditemukan?
