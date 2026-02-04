70422502b5c4d6197afcceb92c0a51f5ef19691c
Mito <michoefanly@gmail.com>
2026-02-04 20:20:48 +0700

    Create Technology.py

---
+++ Technology.py
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Web anti kecanduan teknologi modern</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <iframe src="https://www.meteoblue.com/en/weather/widget/daily/ip_romania_675514?geoloc=fixed&tempunit=CELSIUS&windunit=KILOMETER_PER_HOUR&precipunit=MILLIMETER&days=4&coloured=coloured&pictoicon=1&maxtemperature=1&mintemperature=1&windspeed=1&windgust=0&winddirection=1&uv=0&humidity=0&precipitation=1&precipitationprobability=1&spot=1&pressure=0&layout=light&user_key=c4c02d7826fb82cb&embed_key=4d9ae591b85f374b&sig=c5db5adbfa9e5125c7b31a36e6ebbb43af39577a48b58a55d3c3e051c5e13b8c" frameborder="0" scrolling="NO" allowtransparency="true" sandbox="allow-same-origin allow-scripts allow-popups allow-popups-to-escape-sandbox" style="width: 216px;height: 349px;border: 0;overflow: hidden;"></iframe><div><!-- DO NOT REMOVE THIS LINK --><a href="https://www.meteoblue.com/en/weather/week/index" target="_blank" rel="noopener">meteoblue</a></div>
+    font-family: Arial,  sans-serif; //
+    font-size: 11px; / 12<p></p> /
+    background-color: #f0f0f0; //
+    color: #333333; /black/ 
+    <h1>Teknologi Sekarang:</h1>
+    <p>Teknologi adalah salah satu aspek yang paling berkembang dan baik untuk kita. Tapi, teknologi tidak sepenuhnya baik, jika kita pakai dengan cara yang salah layaknya mengsalah-gunakan pisau, bisa membahayakan orang. Teknologi juga sama. Jika kamu terlalu sering menyah-gunakan teknologi kamu juga akan dapat beberapa efek samping seperti:</p>
+    <h2> Contoh: </h2>
+             <ul>
+                     <li>Mengscroll media sosial tanpa akhir,
+        
+                     <li>Belanja online,
+                     
+                     <li>Menyontek dengan AI,
+                     
+                     <li>Bermain game terus menerus.
+             </ul>
+     <img src="https://1.bp.blogspot.com/-qR0itpiafCM/WI0xctSt_1I/AAAAAAAAAnA/cH05jcZ4fhc-WelLQSCHR9VitH3rvVa0wCLcB/s1600/Radiasi%2BWifi%2B.jpg">
+</body>
+
+</html>
+
